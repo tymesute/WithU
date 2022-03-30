2c54143084c0677a4f4f8ef1176199fa2c1773f4
Tymeee <88166167+Tymeee@users.noreply.github.com>
2022-03-30 13:02:55 +0700

    Initial Commit

---
+++ CryingCloud/CryingCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryingCloudApp.swift
+//  CryingCloud
+//
+//  Created by E2318556 on 30/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct CryingCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
