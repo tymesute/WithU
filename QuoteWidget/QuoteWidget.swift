7d8b9f795c14bffcab8743b54793890481393b2b
Tymeee <tyme.suteesopon@gmail.com>
2024-03-15 00:25:37 -0400

    Fix widget containerBackgrounD API

--- QuoteWidget/QuoteWidget.swift
+++ QuoteWidget/QuoteWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), quotes: "1")
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), quotes: "1")
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 1 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate, quotes: MyDataProvider.getQuotes())
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -41,19 +41,33 @@ struct SimpleEntry: TimelineEntry {
 
 struct QuoteWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Image(entry.quotes)
-            .resizable()
-            .scaledToFill()
-            .ignoresSafeArea(.all)
+        ZStack{
+            Color.white.edgesIgnoringSafeArea(.all)
+            Image(entry.quotes)
+                .resizable()
+                .ignoresSafeArea()
+        }.widgetBackground(Color.white)
+    }
+}
+
+extension View {
+    func widgetBackground(_ backgroundView: some View) -> some View {
+        if #available(iOSApplicationExtension 17.0, *) {
+            return containerBackground(for: .widget) {
+                backgroundView
+            }
+        } else {
+            return background(backgroundView)
+        }
     }
 }
 
 @main
 struct QuoteWidget: Widget {
     let kind: String = "QuoteWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             QuoteWidgetEntryView(entry: entry)
@@ -61,6 +75,7 @@ struct QuoteWidget: Widget {
         .configurationDisplayName("WithU by Crying Cloud")
         .description("ตกแต่งหน้าจอโฮมด้วยข้อความเพิ่มกำลังใจ")
         .supportedFamilies([.systemSmall, .systemLarge])
+        .contentMarginsDisabled()
     }
 }
 
@@ -74,7 +89,7 @@ struct QuoteWidget_Previews: PreviewProvider {
 class MyDataProvider{
     static func getQuotes() -> String{
         let quotes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146", "147", "148", "149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192", "193", "194", "195", "196", "197", "198", "199"]
-                      
+        
         
         return quotes.randomElement()!
         
